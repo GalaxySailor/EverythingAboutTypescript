891b5588bc7f57e4821a4c74c4a7ef6a7ba5a21b
Moses Kim <crystal.galaxy.sailor@gmail.com>
2020-09-20 17:09:06 -0700

    Initial commit

---
+++ index.js
@@ -0,0 +1,5 @@
+var n1 = 5, n2 = 4;
+var add = function (num1, num2) {
+    return num1 + num2;
+};
+console.log(add(n1, n2));
---
+++ index.ts
@@ -0,0 +1,8 @@
+const n1: number = 5,
+  n2: number = 4;
+
+const add = (num1: number, num2: number) => {
+  return num1 + num2;
+};
+
+console.log(add(n1, n2));
